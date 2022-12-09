1d90c83dbadeb426f6b9fd2b9df11eff66ddda4b
Caitlin Buen-Lucas <cbuenlucas@gmail.com>
2022-12-09 14:44:30 -0800

    created a simple form, without ability to submit

--- react-kitchen/src/App.jsx
+++ react-kitchen/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
 // import reactLogo from './assets/react.svg';
 import { Orders } from './components/Orders';
+import { OrderForm } from './components/OrderForm';
 import './App.css';
 
 function App() {
-  const [state, setState] = useState({
+
+  const allOrders = {
     completeOrders: {
       1: {
         id: 1,
@@ -23,23 +25,34 @@ function App() {
         isComplete: "false",
       },
     },
-  });
+  }
+
+  const [state, setState] = useState(allOrders);
+
+  const createOrder = (order) => {
+    setState(...allOrders, allOrders.incompleteOrders.order);
+  }
 
   return (
     <div className="App">
-      {Object.keys(state.incompleteOrders).length ?
-        <div className="orders" id="incomplete-orders">
-          <span className="headers">Incomplete orders:</span>
-          <Orders orders={Object.values(state.incompleteOrders)} />
+      <div>
+        <OrderForm createOrder={createOrder} />
+      </div>
+      <div>
+        {Object.keys(state.incompleteOrders).length ?
+          <div className="orders" id="incomplete-orders">
+            <span className="headers">Incomplete orders:</span>
+            <Orders orders={Object.values(state.incompleteOrders)} />
+          </div>
+          :
+          ""
+        }
+      <br></br>
+      <br></br>
+        <div className="orders" id="complete-orders">
+          <span className="headers">Complete orders:</span>
+          <Orders orders={Object.values(state.completeOrders)} />
         </div>
-        :
-        ""
-      }
-      <br></br>
-      <br></br>
-      <div className="orders" id="complete-orders">
-        <span className="headers">Complete orders:</span>
-        <Orders orders={Object.values(state.completeOrders)} />
       </div>
     </div>
   )
--- react-kitchen/src/components/OrderForm.jsx
+++ react-kitchen/src/components/OrderForm.jsx
@@ -1,5 +1,47 @@
 import React, { useState } from "react";
+import "./OrderForm.css"
 
-// export const OrderForm = () => {
+export const OrderForm = ({ createOrder }) => {
 
-// }
+    const [newOrder, setNewOrder] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        createOrder(newOrder);
+    }
+
+    return (
+        <>
+            <form className="form" onSubmit={handleSubmit}>
+                <p id="form-header">Create your order:</p>
+                    <div className="inner-form">
+                        <label className="labels">
+                            Name:&nbsp;
+                            <input
+                                className="inputs"
+                                type="text"
+                            // value={}
+                            />
+                        </label>
+                        <br></br>
+                        <label className="labels">
+                            Address:&nbsp;
+                            <input
+                                className="inputs"
+                                type="text"
+                            />
+                        </label>
+                        <br></br>
+                        <label className="labels">
+                            Items:&nbsp;
+                            <input
+                                className="inputs"
+                                type="text"
+                            />
+                        </label>
+                    </div>
+            </form>
+        </>
+    )
+}
